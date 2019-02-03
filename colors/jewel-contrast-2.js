335fbdb35e99303e5ab8140c711a1b1ed98f7b6e
Marcos <marcos0x@gmail.com>
2019-02-03 16:49:24 -0300

    Adds jewel-contrast-2 color

---
+++ colors/jewel-contrast-2.js
@@ -0,0 +1,24 @@
+module.exports = {
+    backgroundColor:  "#111",
+    foregroundColor:  "#fff",
+    borderColor:      "#282b2f",
+    cursorColor:      "#ffffff",
+    colors: {
+        black:        "#1f2227", // #1f2227
+        red:          "#ba0e2e", // #ba0e2e
+        green:        "#62c47c", // #6299c4
+        yellow:       "#c262c4", // #c262c4
+        blue:         "#6299c4", // #62c47c
+        magenta:      "#62c47c", // #6299c4
+        cyan:         "#6299c4", // #62c47c
+        white:        "#d0d9e4", // #d0d9e4
+        lightBlack:   "#3d4551", // #363b43
+        lightRed:     "#e61f44", // #e61f44
+        lightGreen:   "#acc9e0", // #acc9e0
+        lightYellow:  "#dface0", // #dface0
+        lightBlue:    "#ace0ba", // #ace0ba
+        lightMagenta: "#acc9e0", // #acc9e0
+        lightCyan:    "#ace0ba", // #ace0ba
+        lightWhite:   "#f1f3f7" // #f1f3f7
+    }
+};
